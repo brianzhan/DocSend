714935d0753923d8333fdcc7847dc05b4f05c46d
brianzhan <brianjzhan@gmail.com>
2025-06-04 13:38:35 -0700

    Add instructions for loading the extension

---
+++ extension/README.md
@@ -0,0 +1,13 @@
+# DocSend Slide Deck Downloader Extension
+
+This folder contains a Chrome extension that converts DocSend slide decks into a single PDF file.
+
+## Loading the extension
+
+Chrome requires the manifest file to be at the root of the extension folder. All extension files are located in the `extension/` directory of this repository.
+
+1. Open `chrome://extensions` in Chrome.
+2. Enable **Developer mode**.
+3. Click **Load unpacked** and select the `extension` folder of this repository.
+4. Navigate to a DocSend slide deck and click the extension button to download the slides as a PDF.
+
---
+++ extension/background.js
@@ -0,0 +1,62 @@
+let connection;
+let jobInProgress = false;
+let jobComplete = false;
+
+const executeJob = () => {
+    jobInProgress = true;
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const currentTabId = tabs[0].id;
+        chrome.scripting.executeScript({
+            target: {tabId: currentTabId},
+            files: ["./modules/pdfkit.js", "./modules/blob-stream.js", "./src/DocSendDownloader.js"]
+        }, () => {
+            connection = chrome.tabs.connect(currentTabId);
+            connection.postMessage({requestType: "GENERATE_PDF"});
+            connection.onMessage.addListener((message) => {
+                if (message.requestType == "SET_JOB_COMPLETE") {
+                    jobInProgress = false;
+                    jobComplete = true;
+                }
+            });
+        });
+    });
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: {hostSuffix: 'docsend.com', pathContains: 'view'},
+                    })
+                ],
+                actions: [new chrome.declarativeContent.ShowAction()]
+            }
+        ]);
+    });
+});
+
+chrome.action.onClicked.addListener(() => {
+    chrome.webRequest.onHeadersReceived.addListener(
+        function(response) {
+            response.responseHeaders.push({'name': "Access-Control-Allow-Origin", 'value': "*"});
+            response.responseHeaders.push({'name': "Access-Control-Allow-Methods", 'value': "GET, OPTIONS"});
+            return {responseHeaders: response.responseHeaders};
+        },
+        {
+            urls: ["https://*.docsend.com/*", "https://*.cloudfront.net/*"]
+        },
+        ["blocking", "responseHeaders", "extraHeaders"]
+    );
+
+    if (jobComplete || jobInProgress) {
+        try {
+            connection.postMessage({requestType: "CHECK_PROGRESS"});
+        } catch {
+            executeJob();
+        }
+    } else {
+        executeJob();
+    }
+});
